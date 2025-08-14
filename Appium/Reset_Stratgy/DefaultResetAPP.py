d0ec524d22dc4ce9201670b93545b77d253bf082
Aditya-41 <adityapatil0226@gmail.com>
2025-08-14 18:11:58 +0530

    Reset Stratery of Device

---
+++ Appium/Reset_Stratgy/DefaultResetAPP.py
@@ -0,0 +1,27 @@
+from appium import webdriver
+from appium.options.android import UiAutomator2Options
+from appium.webdriver.appium_service import AppiumService
+import time
+ 
+# appium_service = AppiumService()
+# appium_service.start()
+
+desired_caps = {}
+desired_caps['platformName'] = 'Android'
+desired_caps['deviceName'] = 'pixel6a'
+desired_caps['udid'] = 'emulator-5554'
+desired_caps['automationName'] = 'UiAutomator2'
+desired_caps['appPackage'] = 'com.android.chrome'   
+desired_caps['appActivity'] = 'com.google.android.apps.chrome.Main'
+# desired_caps['noReset'] = True  # Set noReset to True
+
+options = UiAutomator2Options().load_capabilities(desired_caps)
+options.set_capability("ignoreHiddenApiPolicyError", True)
+
+driver = webdriver.Remote('http://127.0.0.1:4723', options=options,direct_connection=True)
+# driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", desired_caps)
+ 
+time.sleep(5)
+ 
+driver.quit()
+# appium_service.stop()
