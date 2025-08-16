984e819830d5b552b24080b8b33d6e5b281fdaa5
Aditya-41 <adityapatil0226@gmail.com>
2025-08-16 14:56:09 +0530

    Added Element Actions and .gitignore file

---
+++ Appium/Element_property/eLe_prop.py
@@ -0,0 +1,44 @@
+from appium import webdriver
+from appium.options.android import UiAutomator2Options
+from appium.webdriver.appium_service import AppiumService
+from appium.webdriver.common.appiumby import AppiumBy
+import time
+ 
+# appium_service = AppiumService()
+# appium_service.start()
+
+desired_caps = {}
+desired_caps['platformName'] = 'Android'
+desired_caps['deviceName'] = 'pixel6a'
+desired_caps['udid'] = 'emulator-5554'
+desired_caps['automationName'] = 'UiAutomator2'
+desired_caps['appPackage'] = 'com.code2lead.kwad'   
+desired_caps['appActivity'] = 'com.code2lead.kwad.MainActivity'
+
+
+options = UiAutomator2Options().load_capabilities(desired_caps)
+options.set_capability("ignoreHiddenApiPolicyError", True)
+
+driver = webdriver.Remote('http://127.0.0.1:4723', options=options,direct_connection=True)
+# driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", desired_caps)
+ 
+
+
+# Select Element by Index value 
+element = driver.find_element(AppiumBy.XPATH, "//android.widget.Button[@content-desc='Btn1']")
+time.sleep(5)
+
+# print("Element found:", ele_id.text)
+print("Text:", element.text)                           # visible text
+print("Class:", element.get_attribute("className"))    # class of element
+print("Content-desc:", element.get_attribute("contentDescription"))  
+print("Enabled:", element.get_attribute("enabled"))    
+print("Displayed:", element.is_displayed())            
+print("Selected:", element.is_selected())              
+print("Clickable:", element.get_attribute("clickable"))
+print("Bounds:", element.get_attribute("bounds")) 
+ 
+driver.press_keycode(3)  # Press Enter Home key
+time.sleep(5)
+driver.quit()
+# appium_service.stop()
