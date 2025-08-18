689b6e66018e75128a9ef96a5adab40074dcc51b
Aditya-41 <adityapatil0226@gmail.com>
2025-08-18 16:36:08 +0530

    pythong Logging

--- Python_Logging/Logging.py
+++ Python_Logging/Logging.py
@@ -1,22 +1,3 @@
-### Logging means tracking of the event or step     during the execution of programe or software
-## Log Level
-
-* Default Info and Debug are Disabled
-
-
-1. Critical
-2. Error
-3. Warning 
-4. Info 
-5. Debug
-
-
-## Example
-* 29/03/2024 03:48:17 PM Sunday: INFO: Text Entered
-* 29/03/2024 03:48:17 PM Sunday: WARNING: Text Entered
-
-
-```python
 import logging
 import datetime
 
